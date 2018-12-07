bf9169c2b0d8280bad595491dbe2bdf2b855cb82
haha <15638590102@163.com>
2018-12-07 09:20:48 +0800

    实体类

--- iep_exam/src/main/java/com/ysd/iep/entity/Answer.java
+++ iep_exam/src/main/java/com/ysd/iep/entity/Answer.java
@@ -22,12 +22,12 @@ import javax.persistence.Table;
 public class Answer {
     @Id
     @Column(name = "Id", nullable = false, length = 20)
-    private Integer Id;
+    private Integer Id;//答案id
     @Column(name = "Option", nullable = false, length = 50)
-    private String Option;
+    private String Option;//选项(ABCD)
     @Column(name = "Content", nullable = false, length = 50)
-    private String Content;
+    private String Content;//选项内容
     @Column(name = "RubricId", nullable = false, length = 50)
-    private String RubricId;
+    private String RubricId;//题干id
 
 }
