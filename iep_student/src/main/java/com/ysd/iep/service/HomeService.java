0f1efc28119d09ab7e35805dc4a4a4fcf7fb7a33
Jie <18790943013@163.com>
2018-12-10 15:55:17 +0800

    sda

---
+++ iep_student/src/main/java/com/ysd/iep/service/HomeService.java
@@ -0,0 +1,14 @@
+package com.ysd.iep.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+/**
+ * 调大壮服务
+ * @author ASUS
+ *
+ */
+@FeignClient("")
+public class HomeService {
+	
+	
+
+}
