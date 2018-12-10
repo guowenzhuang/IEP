0b4866a3ec8e99d92dcc0752469ba88b9f589186
guowenzhuang <80795289@qq.com>
2018-12-10 10:52:08 +0800

    admin权限创建

---
+++ iep_admin/src/main/java/com/ysd/iep/AdminApplication.java
@@ -0,0 +1,18 @@
+package com.ysd.iep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author 80795
+ * @date 2018/11/12 8:55
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class AdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class,args);
+    }
+
+}
---
+++ iep_admin/src/main/java/com/ysd/iep/entity/po/DepartmentDB.java
@@ -0,0 +1,25 @@
+package com.ysd.iep.entity.po;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+/**
+ * 院系
+ * @author 80795
+ * @date 2018/11/12 8:55
+ */
+@Entity
+@Table(name = "department", schema = "ip-permission", catalog = "")
+@Data
+@Accessors(chain = true)
+public class DepartmentDB {
+    @Id
+    @Column(name = "departmentId", nullable = false, length = 100)
+    @GenericGenerator(name="idGenerator", strategy="uuid")
+    @GeneratedValue(generator="idGenerator")
+    private String departmentId;
+    private String name;
+}
