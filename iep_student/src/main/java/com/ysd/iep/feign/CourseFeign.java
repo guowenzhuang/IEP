152c090b36e1d17ec6f491d21ff430b83c367400
zmzuimei <15978827730@163.com>
2018-12-13 21:08:17 +0800

    课程ID查询

--- iep_student/src/main/java/com/ysd/iep/feign/CourseFeign.java
+++ iep_student/src/main/java/com/ysd/iep/feign/CourseFeign.java
@@ -3,9 +3,11 @@ package com.ysd.iep.feign;
 import com.ysd.iep.util.Result;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient("IEP-STUDENT")
+@FeignClient("IEP-TEACHER")
 public interface CourseFeign {
+
     @RequestMapping("/course/getCourUIPage")
-    Result getCourseAll(String depId,Integer page, Integer size);
+    Result getCourseAll(@RequestParam("depId")String depId, @RequestParam("page")Integer page, @RequestParam("size")Integer size);
 }
