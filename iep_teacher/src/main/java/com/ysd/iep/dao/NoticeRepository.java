7a6063607446ecccf79178f3494bfe018f718808
X-秋妍 <X-@DESKTOP-OAJV5A4>
2018-12-13 21:28:36 +0800

    根据课程查询公告

--- iep_teacher/src/main/java/com/ysd/iep/dao/NoticeRepository.java
+++ iep_teacher/src/main/java/com/ysd/iep/dao/NoticeRepository.java
@@ -1,9 +1,13 @@
 package com.ysd.iep.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.ysd.iep.entity.notice;
 
 public interface NoticeRepository extends JpaRepository<notice, Integer>{
-
+	 @Query(value = "SELECT n.* FROM notice n LEFT JOIN course c ON n.noCourid=c.courId WHERE c.courId=2", nativeQuery = true)
+		public List<notice> queryNoticeByCourId(Integer courId);
 }
