27d73b33f5faeee07e28ba602be734d29747f897
Jie <18790943013@163.com>
2018-12-13 21:29:24 +0800

    更新

--- iep_student/src/main/java/com/ysd/iep/feign/CourseFeign.java
+++ iep_student/src/main/java/com/ysd/iep/feign/CourseFeign.java
@@ -4,7 +4,7 @@ import com.ysd.iep.util.Result;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient("IEP-STUDENT")
+@FeignClient("IEP-TEACHER")
 public interface CourseFeign {
     @RequestMapping("/course/getCourUIPage")
     Result getCourseAll(String depId,Integer page, Integer size);
