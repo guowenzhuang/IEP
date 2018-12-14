5887be6a728ee26c86d9dad0cb40a85dbeaa8fae
guowenzhuang <80795289@qq.com>
2018-12-14 16:12:12 +0800

    ssss

---
+++ iep_admin/src/main/java/com/ysd/iep/feign/TeacherFeign.java
@@ -0,0 +1,11 @@
+package com.ysd.iep.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+/**
+ * @author 80795
+ * @date 2018/11/12 8:55
+ */
+@FeignClient("")
+public class TeacherFeign {
+}
