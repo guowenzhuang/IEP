5ca38425b6c855d876533e2e63b1be8bf7bc9697
cuiyijie <980880304@qq.com>
2018-12-15 11:58:14 +0800

    调用teacher根据教师id查询所有课程的接口

---
+++ iep_exam/src/main/java/com/ysd/iep/feign/TeacherFeign.java
@@ -0,0 +1,16 @@
+package com.ysd.iep.feign;
+
+import com.ysd.iep.util.Result;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@FeignClient(value = "IEP-TEACHER")
+public interface TeacherFeign {
+    @GetMapping("/course/queryCourByteaId")
+    Result<List<Course>> getCouse(@RequestParam("teaId") String teaId);
+
+}
