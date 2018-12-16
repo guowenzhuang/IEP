78609225cb0f6c055a0dec0afd3a5550f6835ee7
baitingting <18339043733@163.com>
2018-12-16 18:48:46 +0800

    查询章节

---
+++ iep_teacher/src/main/java/com/ysd/iep/service/ChapterService.java
@@ -0,0 +1,12 @@
+package com.ysd.iep.service;
+
+import java.util.List;
+
+import com.ysd.iep.entity.Chapters;
+
+public interface ChapterService {
+	/**
+	 * 查询章节
+	 */
+	public List<Chapters> querychapterTree();
+}
