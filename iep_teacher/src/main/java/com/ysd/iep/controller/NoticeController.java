5c7bd09ab509526c1808725319e92a4582a4f28f
Jie <18790943013@163.com>
2018-12-23 13:44:13 +0800

    更新

--- iep_teacher/src/main/java/com/ysd/iep/controller/NoticeController.java
+++ iep_teacher/src/main/java/com/ysd/iep/controller/NoticeController.java
@@ -1,19 +1,18 @@
+
 package com.ysd.iep.controller;
 
-import java.util.List;
-
+import com.ysd.iep.entity.dto.Result;
+import com.ysd.iep.entity.notice;
+import com.ysd.iep.service.NoticeService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ysd.iep.entity.notice;
-import com.ysd.iep.entity.dto.Result;
-import com.ysd.iep.service.NoticeService;
-
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import java.util.List;
 
 @Api(value="/notice", tags="公告")
 @RestController
@@ -21,7 +20,6 @@ import io.swagger.annotations.ApiOperation;
 public class NoticeController {
 	 @Autowired
 	 private NoticeService noticeService;
-	
 	/**
      * @param courId
      * @return
