45f738ebe5d4d26cf07bef45d275e48765b6bd75
孟劭华 <1340050895@qq.com>
2018-12-24 11:34:18 +0800

    课堂讨论

---
+++ iep_bbs/src/main/java/com/ysd/iep/service/CoursePostService.java
@@ -0,0 +1,11 @@
+package com.ysd.iep.service;
+
+import java.util.List;
+
+import com.ysd.iep.entity.CoursePost;
+
+public interface CoursePostService {
+	
+	public List<CoursePost> queryAllCoursePost();
+
+}
---
+++ iep_bbs/src/main/java/com/ysd/iep/serviceImpl/CoursePostServiceImpl.java
@@ -0,0 +1,16 @@
+package com.ysd.iep.serviceImpl;
+
+import java.util.List;
+
+import com.ysd.iep.entity.CoursePost;
+import com.ysd.iep.service.CoursePostService;
+
+public class CoursePostServiceImpl implements CoursePostService{
+
+	@Override
+	public List<CoursePost> queryAllCoursePost() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
