ea34ac03b55a931a47b9b25b8443ce29f484226c
baitingting <18339043733@163.com>
2019-01-03 12:00:01 +0800

    学生+课程

--- iep_student/src/main/java/com/ysd/iep/feign/AdminFeign.java
+++ iep_student/src/main/java/com/ysd/iep/feign/AdminFeign.java
@@ -1,9 +1,13 @@
 package com.ysd.iep.feign;
 
+import com.ysd.iep.entity.dto.StudentUserDTO;
 import com.ysd.iep.entity.dto.UsersDTO;
 import com.ysd.iep.util.Result;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -16,4 +20,15 @@ import java.util.List;
 public interface AdminFeign {
     @GetMapping("/user/getUserById")
     Result<List<UsersDTO>> get(@RequestParam("ids") String ids);
+    
+    /**
+     * 根据用户id获取用户信息
+     * @param ids
+     * @return
+     */
+    @GetMapping("/user/getUserById")
+    Result<List<StudentUserDTO>> getUserById(@RequestParam("ids") String ids);
+    
+    @PutMapping("/user/updateUserById/{id}")
+    Result<String> updateUserById(@PathVariable("id") String id,@RequestBody StudentUserDTO user);
 }
