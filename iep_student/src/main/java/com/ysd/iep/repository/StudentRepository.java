bc5112402169b0725ccb33b42a5a91962a4f0603
Jie <18790943013@163.com>
2019-01-03 19:28:16 +0800

    更新接口

--- iep_student/src/main/java/com/ysd/iep/repository/StudentRepository.java
+++ iep_student/src/main/java/com/ysd/iep/repository/StudentRepository.java
@@ -3,7 +3,10 @@ package com.ysd.iep.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ysd.iep.entity.Student;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface StudentRepository extends JpaRepository<Student, String> {
@@ -15,5 +18,11 @@ public interface StudentRepository extends JpaRepository<Student, String> {
 	 * @return
 	 */
 	public Student findBySid(String sid);
+
+
+	@Query(value = "UPDATE studenttb SET credits=credits+?1 WHERE sid=?2",nativeQuery=true)
+	@Modifying
+	@Transactional
+	public void updateCredits(Integer credits,String sid);
 
 }
