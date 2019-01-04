63ca24662ca58f1d8705e54da62611e0f6fec358
SuperheroMovie00 <15638590102@163.com>
2019-01-04 13:58:54 +0800

    哈哈哈

--- iep_exam/src/main/java/com/ysd/iep/service/ExamparperServiceEr.java
+++ iep_exam/src/main/java/com/ysd/iep/service/ExamparperServiceEr.java
@@ -13,6 +13,7 @@ import java.util.List;
  * @date 2018/12/16
  * 考试试卷Service
  */
+
 public interface ExamparperServiceEr {
 
     /**
@@ -41,6 +42,7 @@ public interface ExamparperServiceEr {
     /**
      * 查看考试结束的考试卷子列表(此学生参加过的考试卷子)
      */
+
     List<Examparper> queryexamendparperwherestudentid(String studentid, Integer courseid);
 
 }
